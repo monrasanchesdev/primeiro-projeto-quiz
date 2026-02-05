62982ced34985002c906ca051796c34e36a083eb
Ramon Leão <monrasanchesl.dev@gmail.com>
2026-02-05 17:48:34 +0000

    att d.a

---
+++ script.js
@@ -0,0 +1,20 @@
+const questions = [
+    {
+        question: "1- Qual é a principal função do JavaScript em uma página web?",
+        answers: [
+            { text: "Definir o estilo visual da página", correct: false },
+            { text: "Criar a estrutura do site", correct: false },
+            { text: "Tornar a página interativa e dinâmica", correct: true },
+            { text: "Gerenciar o banco de dados", correct: false }
+        ]
+    },
+    {
+        question: "2- Qual é a forma correta de declarar uma variável em JavaScript moderno?",
+        answers: [
+            { text: "A- variable x = 10;", correct: false },
+            { text: "JavaScript", correct: false },
+            { text: "CSS", correct: true },
+            { text: "Python", correct: false }
+        ]
+    }
+];
